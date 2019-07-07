147543a963ea8e7dc2916bc2f65caa9efdbd577c
guoqi <uniqueguoqi@gmail.com>
2019-07-07 10:08:44 +0800

    fix

--- gist/access_order.cpp
+++ gist/access_order.cpp
@@ -11,18 +11,23 @@ public:
     int z;
 };
 
+union helper {
+    int A::* p;
+    long offset;
+};
 
 template<typename A, typename data1, typename data2>
 void access_order(data1 A::*mem1, data2 A::*mem2)
 {
-    auto p1 = mem1;
-    auto p2 = mem2;
-    string result = p1 > p2 ? "first occurs first"  : "first occurs second";
+    helper p1 = helper{mem1};
+    helper p2 = helper{mem2};
+    string result = p1.offset > p2.offset ? "first occurs first"  : "first occurs second";
     cout << result << endl;
 }
 
 int main()
 {
     access_order(&A::z, &A::y);
+    access_order(&A::x, &A::y);
     return 0;
 }
